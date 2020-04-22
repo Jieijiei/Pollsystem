a947eb978f0c5059cdbaa9e5133c6cee896e9890
Jiei <hamuhamueiji@gmail.com>
2020-04-22 20:48:05 +0900

    モデル作成

--- Poll/models.py
+++ Poll/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Work(models.Model):
+    title = models.TextField()
+    creator_name = models.TextField()
+    thumbnail = models.ImageField()
+    polled = models.IntegerField()
